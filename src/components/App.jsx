bfbd81815fac98dba4a46fb4b1f035f8b8e8a700
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-08 19:10:05 +0200

    sections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,66 @@
+import { Container } from './index.styled';
+import { Section } from './Section/Section';
+
 export const App = () => {
   return (
     <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
+      style={
+        {
+          // height: '100vh',
+          // marginTop: 20,
+          // display: 'flex',
+          // justifyContent: 'center',
+          // alignItems: 'center',
+          // fontSize: 40,
+          // color: '#010101',
+          // borderBottom: '1px solid rgb(212, 212, 212)',
+        }
+      }
     >
-      React homework template
+      <Container>
+        <Section title="Please leave feedback"></Section>
+        <Section title="Statistics"></Section>
+      </Container>
     </div>
   );
 };
+
+{
+  /* <div className="Container">
+  <section>
+    <h1>Section Title</h1>
+    <p>Some content related to this section.</p>
+  </section>
+  ;
+  <section>
+    <h1>Section Title</h1>
+    <p>Some content related to this section.</p>
+  </section>
+  ;
+</div>; */
+}
+
+// import Header from './1-Header/Header';
+// import Content from './2-Content/Content';
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         // display: 'flex',
+//         // justifyContent: 'center',
+//         // alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101',
+//         marginTop: 20,
+//         marginLeft: 20,
+//         // textTransform: 'uppercase',
+//       }}
+//     >
+//       <span style={{ textTransform: 'uppercase' }}>react-module-1 :</span>
+//       <Header />
+//       <Content />
+//     </div>
+//   );
+// };
---
+++ src/components/Section/Section.styled.js
@@ -0,0 +1,23 @@
+import { styled } from 'styled-components';
+
+// export const Container = styled.div`
+//   background-color: rgba(104, 133, 176, 0.7);
+//   border-radius: 8px;
+//   box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.7);
+//   color: #fff;
+//   margin: 10px auto;
+//   padding: 15px;
+//   width: 300px;
+// `;
+
+export const SectionTitle = styled('h2')({
+  fontSize: '28px',
+  textShadow: '1px 3px 0 #505050, -1px 3px 0 #505050',
+  letterSpacing: '0.9px',
+});
+
+export const SectionBox = styled('section')({
+  //   paddingTop: '10px',
+  //   paddingBottom: '40px',
+  //   textAlign: 'center',
+});
