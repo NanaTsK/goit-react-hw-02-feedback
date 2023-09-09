557f75dc17f6dab8049271edba6dd27d4aafbff8
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-09 14:31:28 +0200

    feedback

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Container } from './index.styled';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Component } from 'react';
+import { Statistics } from './Statistics/Statistics';
 import { Notification } from './Notification/Notification';
 
 export class App extends Component {
@@ -11,7 +12,28 @@ export class App extends Component {
     bad: 0,
   };
 
+  handlerFeedback = (event, option) => {
+    this.setState(prevState => {
+      return {
+        [option]: prevState[option] + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = (good, neutral, bad) => good + neutral + bad;
+
+  countPositiveFeedbackPercentage(good, total) {
+    return Math.round((good / total) * 100);
+  }
+
   render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback(good, neutral, bad);
+    const positivePercentage = this.countPositiveFeedbackPercentage(
+      good,
+      total
+    );
+
     return (
       <Container>
         <Section title="We'd love your feedback">
@@ -21,24 +43,19 @@ export class App extends Component {
           />
         </Section>
         <Section title="Statistics">
-          <Notification message="There is no feedback"></Notification>
+          {total ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
         </Section>
       </Container>
     );
   }
 }
-
-// FeedbackOptions = ({ options, onLeaveFeedback
-
-/* <div className="Container">
-  <section>
-    <h1>Section Title</h1>
-    <p>Some content related to this section.</p>
-  </section>
-  ;
-  <section>
-    <h1>Section Title</h1>
-    <p>Some content related to this section.</p>
-  </section>
-  ;
-</div>; */
