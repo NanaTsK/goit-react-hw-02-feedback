b38dd02f3cefccd1886bd70e8d5de673a1b15c29
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-09 14:23:58 +0200

    statistics

---
+++ src/components/Statistics/Statistics.styled.js
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+export const StatisticsList = styled('ul')({
+  //   display: 'flex',
+  //   justifyContent: 'center',
+  //   gap: '15px',
+});
+
+export const StatisticsText = styled('p')({
+  fontSize: '18px',
+  fontWeight: 500,
+});
