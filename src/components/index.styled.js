b139f8f51340fbf23e4399c917f06b8608e087a8
NanaTsK <126491413+NanaTsK@users.noreply.github.com>
2023-09-09 23:06:20 +0200

    container

--- src/components/index.styled.js
+++ src/components/index.styled.js
@@ -1,7 +1,7 @@
 import { styled } from 'styled-components';
 
 export const Container = styled.div`
-  background-color: rgba(104, 133, 176, 0.5);
+  background-color: rgba(104, 133, 176, 0.8);
   border-radius: 4px;
   box-shadow: 0px 0px 2px 1px rgba(0, 0, 0, 0.6);
   color: #fff;
